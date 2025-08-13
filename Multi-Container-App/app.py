e710fa28873252b1bb93468e157ccefc790c39db
abdullahiabdullahi1002-cloud <abdullahi.abdullahi1002@gmail.com>
2025-08-13 23:20:35 +0100

    Added static and template files

--- Multi-Container-App/app.py
+++ Multi-Container-App/app.py
@@ -1,28 +1,28 @@
-# app.py
-from flask import Flask
+from flask import Flask, render_template
 import redis
 import os
 
 app = Flask(__name__)
 
-# Redis connection settings from environment variables
-redis_host = os.getenv("REDIS_HOST", "localhost")
-redis_port = int(os.getenv("REDIS_PORT", 6379))
-
+# Redis connection using environment variables
 r = redis.Redis(
-    host=redis_host,
-    port=redis_port,
+    host=os.getenv('REDIS_HOST', 'redis'),
+    port=int(os.getenv('REDIS_PORT', 6379)),
     decode_responses=True
 )
 
 @app.route('/')
 def home():
-    return "Welcome to the Flask + Redis app!"
+    return render_template('home.html')
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
 
 @app.route('/count')
 def count():
-    visits = r.incr("visits")
-    return f"Visit count: {visits}"
+    visits = r.incr("visits")  # increment the counter
+    return render_template('count.html', count=visits)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5001)
